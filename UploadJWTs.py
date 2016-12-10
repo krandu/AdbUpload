f78aa1f02c797af271bd31c2535fd2d566513607
dukaiguang <granddu@outlook.com>
2016-12-11 00:54:01 +0800

    UpladJWTs多台机务通同时上传代码完成，可以开始使用。

--- UploadJWTs.py
+++ UploadJWTs.py
@@ -2,28 +2,46 @@
 # 自动刷机务通数据
 import os
 import pygame
-done = False
+import threading
+import re
 count = 0
+adbDevices = []
+devicesNum =  []
+doneDevicesNum = []
+toDoDevicesNum =[]
 
-
-def upload():
-    global done, count
+def upload(deviceNum = ''):
+    print deviceNum + '线程开始上传\n'
+    global count
     # 在此处插入执行的ADB命令语句
-    os.popen("adb uninstall com.tf.elecbook")
-    os.popen("adb install /home/dukaiguang/jwt客户端/elecbook.apk")
-    os.popen("adb push /home/dukaiguang/jwt客户端/畅想机务通/ /sdcard/")
+    os.popen("adb -s %s uninstall com.tf.elecbook"%deviceNum)
+    os.popen("adb -s %s install /home/dukaiguang/jwt客户端/elecbook.apk"%deviceNum)
+    print deviceNum + '线程安装apk完成\n'
+    os.popen("adb -s %s push /home/dukaiguang/jwt客户端/畅想机务通/ /sdcard/"%deviceNum)
+    os.popen("adb -s %s shell reboot"%deviceNum)
     pygame.mixer.music.play()
-    done = 1 # 连接的设备刷新完成标志
     count += 1 # 刷新设备总数计数
-    print ("上传完成一台,请拔下设备,目前总共上传完成%d"%count)
+    print deviceNum + ("上传完成,请拔下设备,目前总共上传完成%d\n"%count)
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.mixer.music.load("/usr/share/sounds/ubuntu/ringtones/Ubuntu.ogg") # 随便找了个音乐做完成提示音
+    # 随便找了个音乐做完成提示音
+    pygame.mixer.music.load("/usr/share/sounds/ubuntu/ringtones/Ubuntu.ogg")
     while True:
-        if (done == False)and((len(os.popen('adb devices').readlines())) == 3):
-            print ("发现设备,开始上传")
-            upload()
-        elif (done == True)and((len(os.popen('adb devices').readlines())) != 3):
-            done = False # 把刷新完成标志清除
-            print ("请插入一台新设备")
+        # 首先清空要刷机的设备列表
+        toDoDevicesNum = []
+        adbDevices = os.popen("adb devices").readlines()
+        # 把安卓设备序号提出来
+        for line in adbDevices[1:-1]:
+            deviceNum = (re.sub(r'\S*\s*\n','',line))
+            # 已经上传过数据的不再刷机
+            if deviceNum not in doneDevicesNum:
+                toDoDevicesNum.append(deviceNum)
+        for line in toDoDevicesNum:
+            # 开启线程把设备序号传给上传程序
+            threadUpload = threading.Thread(target=upload,args=(line,))
+            threadUpload.setDaemon(True)
+            threadUpload.start()
+            print line + '上传线程正在运行中\n'
+            # 把上传过数据的加入列表
+            doneDevicesNum.append(line)
